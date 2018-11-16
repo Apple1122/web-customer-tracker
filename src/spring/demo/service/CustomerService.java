56ea36a0dc94c16fac830bfbe9736f062e19eca1
Kyle Huang <kyle.huang168@gmail.com>
2018-11-16 23:00:04 +0800

    add service layer

---
+++ src/spring/demo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package spring.demo.service;
+
+import java.util.List;
+
+import spring.demo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
---
+++ src/spring/demo/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package spring.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import spring.demo.dao.CustomerDAO;
+import spring.demo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// need to inject customer DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+}
